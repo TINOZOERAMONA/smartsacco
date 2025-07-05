98c607e30d19a7de65d1659eec77bd1c303d90bd
Lisacode100 <eageno075@gmail.com>
2025-07-05 21:47:32 +0300

    added momo payment

--- lib/models/momopayment.dart
+++ lib/models/momopayment.dart
@@ -20,3 +20,49 @@ class MomoPaymentPage extends StatefulWidget {
   State<MomoPaymentPage> createState() => _MomoPaymentPageState();
 }
 
+class _MomoPaymentPageState extends State<MomoPaymentPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _phoneController = TextEditingController();
+  bool _isLoading = false;
+  String? _errorMessage;
+  Timer? _pollingTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    if (kDebugMode) {
+      _phoneController.text = '775123456'; // Test UG number for sandbox
+    }
+  }
+   @override
+  void dispose() {
+    _phoneController.dispose();
+    _pollingTimer?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _processPayment() async {
+    if (!_formKey.currentState!.validate()) return;
+
+    setState(() {
+      _isLoading = true;
+      _errorMessage = null;
+    });
+
+    try {
+      final transactionId = MomoService.generateTransactionId();
+      
+      // Clear previous callback data
+      await _clearCallbackFile();
+
+      final momoService = MomoService(); // Configured with your credentials
+      
+      await momoService.requestPayment(
+        phoneNumber: _phoneController.text,
+        amount: widget.amount,
+        externalId: transactionId,
+        payerMessage: 'SACCO Contribution: UGX ${widget.amount.toStringAsFixed(2)}',
+      );
+
+
+
