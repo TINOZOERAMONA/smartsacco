fa7b008b58f21991a2480d14d7029583e9b598a0
Lisacode100 <eageno075@gmail.com>
2025-07-05 22:01:45 +0300

    loan services credentials

--- lib/services/momoservices.dart
+++ lib/services/momoservices.dart
@@ -15,3 +15,29 @@ class MomoService {
   MomoService() {
     _loadConfig();
   }
+  Future<void> _loadConfig() async {
+    final config = jsonDecode(await rootBundle.loadString(_configFile));
+    _subscriptionKey = config['subscriptionKey'];
+    _apiUser = config['apiUser'];
+    _apiKey = config['apiKey'];
+    _isSandbox = config['isSandbox'];
+    _callbackUrl = config['callbackUrl'];
+  }
+
+  Future<Map<String, dynamic>> requestPayment({
+    required String phoneNumber,
+    required double amount,
+  }) async {
+    final externalId = _generateTransactionId();
+    final url = Uri.parse('$_baseUrl/collection/v1_0/requesttopay');
+
+    final response = await http.post(
+      url,
+      headers: await _headers(externalId),
+      body: jsonEncode(_requestBody(
+        phoneNumber: phoneNumber,
+        amount: amount,
+        externalId: externalId,
+      )),
+    );
+
