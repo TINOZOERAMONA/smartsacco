56ac1ba2f63e5c0d373c957500d7481951a9adcc
Lisacode100 <eageno075@gmail.com>
2025-07-30 12:01:23 +0300

    corrected notification detail

--- lib/models/notification.dart
+++ lib/models/notification.dart
@@ -5,24 +5,10 @@ enum NotificationType {
   general,
 }
 
-class AppNotification {
-  final String id;
-  final String title;
-  final String message;
-  final DateTime date;
-  final NotificationType type;
-  bool isRead;
-  final String? actionUrl;
 
-  AppNotification({
-    required this.id,
-    required this.title,
-    required this.message,
-    required this.date,
-    required this.type,
-    this.isRead = false,
-    this.actionUrl,
-  });
+
+
+
 
   factory AppNotification.fromJson(Map<String, dynamic> json) {
     return AppNotification(
